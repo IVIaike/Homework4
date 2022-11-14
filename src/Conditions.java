ecf03ae545c245dd87a1cb0a948fe067bf1dd253
IVIaike <trainer.ekb@gmail.com>
2022-11-14 23:21:38 +0500

    Задание 1 выполнено

---
+++ src/Conditions.java
@@ -0,0 +1,15 @@
+public class Conditions {
+    public static void main(String[] args) {
+
+        //Задание 1
+        System.out.println("Задание 1");
+        int userAge = 56;
+        if (userAge>=18) {
+            System.out.println("Поздравляем Вас с совершеннолетием!");
+        }
+        if (userAge<18) {
+            System.out.println("Ваше совершеннолетие еще впереди и вам нужно немного подождать!");
+        }
+
+    }
+}

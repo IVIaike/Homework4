5a441c9e354f469d91b2205313404cac7e4b327b
IVIaike <trainer.ekb@gmail.com>
2022-11-14 23:29:24 +0500

    Задание 2 выполнено

--- src/Conditions.java
+++ src/Conditions.java
@@ -3,7 +3,7 @@ public class Conditions {
 
         //Задание 1
         System.out.println("Задание 1");
-        int userAge = 56;
+        int userAge = 21;
         if (userAge>=18) {
             System.out.println("Поздравляем Вас с совершеннолетием!");
         }
@@ -11,5 +11,18 @@ public class Conditions {
             System.out.println("Ваше совершеннолетие еще впереди и вам нужно немного подождать!");
         }
 
+        //Задание 2
+        System.out.println("Задание 2");
+
+        int humanAge = 19;
+        if (humanAge>=7) {
+            System.out.println("Ребенок ходит в школу.");
+        }
+        if (humanAge>=18) {
+            System.out.println("Человек уже закончил школу и может отправляться в университет!");
+        }
+        if (humanAge>=24) {
+            System.out.println("Человек окончил университет и ему пора искать первую работу!");
+        }
     }
 }
